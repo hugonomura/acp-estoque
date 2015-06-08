b6910d5af9e59cc918919ff69bf0d598db33f549
Hugo Nomura <hugonomura@gmail.com>
2015-06-08 00:24:44 -0300

    listagem de produtos e remoção

--- assets/js/estoque.js
+++ assets/js/estoque.js
@@ -8,6 +8,7 @@ $(function(){
 
     $("#novoProduto").submit(function(event){
         var produto = JSON.stringify({
+            Id: tbProdutos.length.toString(),
             Produto: $("#produto").val(),
             Quantidade: $("#quantidade").val().toString(),
             Preco: $("#preco").val().toString(),
@@ -27,17 +28,33 @@ $(function(){
         for(i in tbProdutos){
             var prod = JSON.parse(tbProdutos[i]);
             listaProdutos = listaProdutos.concat("<tr>");
-            listaProdutos = listaProdutos.concat('<td class="product-entry">' + i.toString() + '</td>');
+            listaProdutos = listaProdutos.concat('<td class="product-entry" id="id-produto">' + prod.Id + '</td>');
             listaProdutos = listaProdutos.concat('<td class="product-entry">' + prod.Produto + '</td>');
             listaProdutos = listaProdutos.concat('<td class="product-entry">' + prod.Quantidade + '</td>');
             listaProdutos = listaProdutos.concat('<td class="product-entry">' + prod.Preco + '</td>');
             listaProdutos = listaProdutos.concat('<td class="product-entry">' + prod.Tipo + '</td>');
-            listaProdutos = listaProdutos.concat('<td class="product-entry"><a href="#" class="btn btn-info"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a><a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>');
+            listaProdutos = listaProdutos.concat('<td class="product-entry"><a href="#" class="btn btn-info"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a><a href="#" class="btn btn-danger btn-excluir" alt="' + prod.Id + '"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a></td>');
             listaProdutos = listaProdutos.concat("</tr>");
         }
         $("#listaProdutos").fadeIn( "slow", function() {
             $("#listaProdutos").html(listaProdutos);
         });
         
+    }
+    listarProdutos();
+
+    $(".btn-excluir").click(function(){
+        indice_selecionado = parseInt($(this).attr("alt"));
+        excluirProduto();
+        listarProdutos();
+    });
+
+    function excluirProduto(){
+        console.log("oi");
+        console.log(indice_selecionado.toString());
+        console.log(tbProdutos);
+        tbProdutos.splice(indice_selecionado, 1);
+        console.log(tbProdutos);
+        localStorage.setItem("tbProdutos", JSON.stringify(tbProdutos));
     }
 });
