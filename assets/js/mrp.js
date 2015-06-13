7437b91d294a7c46b30aa9fd2e5a7d95e79f2d43
Hugo Nomura <hugonomura@gmail.com>
2015-06-13 18:57:31 -0300

    permitindo que o usuário quantos itens são necessários no MRP.

--- assets/js/mrp.js
+++ assets/js/mrp.js
@@ -1,3 +1,23 @@
 $(function() {
+    $("#atualizarMrp").click(function(){
+        var bicicletas = parseInt($("#num-bicicletas").val());
+        
+        if(isNaN(bicicletas))
+            return;
 
+        $("#qtd-bicicleta").html("(" + bicicletas + ")");
+
+        var guidao = 1 * bicicletas;
+        $("#qtd-guidao").html("(" + guidao + ")");
+
+        var montagemQuadro = 1 * bicicletas;
+        $("#qtd-montagem-quadro").html("(" + montagemQuadro + ")");
+
+        var roda = 2 * bicicletas;
+        $("#qtd-roda").html("(" + roda + ")");
+
+        var quadro = 1 * bicicletas;
+        $("#qtd-quadro").html("(" + quadro + ")");
+
+    });
 });
